d252ad5ced2d7e18824158eae467b5c25be26a20
markfour <axhfaro@gmail.com>
2022-05-15 04:12:36 +0900

    Initialize

---
+++ SwiftUICheck/SwiftUICheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICheckApp.swift
+//  SwiftUICheck
+//
+//  Created by kazumi hayashida on 2022/05/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
