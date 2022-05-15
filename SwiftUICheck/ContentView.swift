bcb5d6d4230227403703b6b14d6f3165d1342ef0
markfour <axhfaro@gmail.com>
2022-05-15 18:36:56 +0900

    ContentViewを実装した

--- SwiftUICheck/ContentView.swift
+++ SwiftUICheck/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Text("List")
+                Text("Form")
+                Text("VStack")
+                Text("NavigationView")
+            }
+        }
     }
 }
 
---
+++ SwiftUICheck/ListCheckView.swift
@@ -0,0 +1,8 @@
+//
+//  ListCheckView.swift
+//  SwiftUICheck
+//
+//  Created by kazumi hayashida on 2022/05/15.
+//
+
+import Foundation
