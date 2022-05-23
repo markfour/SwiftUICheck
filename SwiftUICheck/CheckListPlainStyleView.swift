7a9457c3559a5f620501bf6ed73fbbf7da3aff61
markfour <axhfaro@gmail.com>
2022-05-23 22:18:13 +0900

    CheckListPlainStyleView を追加した

---
+++ SwiftUICheck/CheckListPlainStyleView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckListPlainStyleView.swift
+//  SwiftUICheck
+//
+//  Created by Markfour on 2022/05/23.
+//
+
+import SwiftUI
+
+struct CheckListPlainStyleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckListPlainStyleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckListPlainStyleView()
+    }
+}
