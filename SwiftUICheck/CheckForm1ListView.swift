1eafc9bcd111bc2cd4dfd72d9ebd8eebb7921cda
markfour <axhfaro@gmail.com>
2022-06-15 02:29:48 +0900

    CheckForm1ListViewを追加した

---
+++ SwiftUICheck/CheckForm1ListView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckForm1ListView.swift
+//  SwiftUICheck
+//
+//  Created by kazumi hayashida on 2022/06/13.
+//
+
+import SwiftUI
+
+struct CheckForm1ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckForm1ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckForm1ListView()
+    }
+}
