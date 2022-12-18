33fb993078a34a60a3ac74fc5d4a5703fe982a62
markfour <axhfaro@gmail.com>
2022-12-19 02:38:06 +0900

    Update CheckForm1ListView.swift

--- SwiftUICheck/CheckForm1ListView.swift
+++ SwiftUICheck/CheckForm1ListView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct CheckForm1ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Notifications")) {
+                Picker("Notify Me About", selection: $notifyMeAbout) {
+                    Text("Direct Messages").tag(NotifyMeAboutType.directMessages)
+                    Text("Mentions").tag(NotifyMeAboutType.mentions)
+                    Text("Anything").tag(NotifyMeAboutType.anything)
+                }
+                Toggle("Play notification sounds", isOn: $playNotificationSounds)
+                Toggle("Send read receipts", isOn: $sendReadReceipts)
+            }
+            Section(header: Text("User Profiles")) {
+                Picker("Profile Image Size", selection: $profileImageSize) {
+                    Text("Large").tag(ProfileImageSize.large)
+                    Text("Medium").tag(ProfileImageSize.medium)
+                    Text("Small").tag(ProfileImageSize.small)
+                }
+                Button("Clear Image Cache") {}
+            }
+        }
     }
 }
 
